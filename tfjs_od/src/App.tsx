4ecaf8537ca2a78172f7ed12f9b41bd9ab1ebf10
ryota0051 <re0061vh@gmail.com>
2022-02-26 22:59:35 +0900

    img and canvas to component

--- tfjs_od/src/App.tsx
+++ tfjs_od/src/App.tsx
@@ -2,45 +2,12 @@ import React, { useState } from "react"
 import { useModels } from "./hooks/useModels"
 import Header from "./components/header"
 import InputFile from "./components/inputFile"
+import DetectedResult from "./components/detectedResult"
 import { Box, ChakraProvider, VStack, Text } from "@chakra-ui/react"
 
 function App() {
   const model = useModels()
   const [imgURL, setImgURL] = useState<string>("")
-  const onImageChange = (e: any) => {
-    const img = e.target as HTMLImageElement
-    const c = document.getElementById("canvas") as HTMLCanvasElement
-    const ctx = c.getContext("2d")
-    if (!ctx) return
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    const font = "16px sans-serif"
-    c.width = img.offsetWidth
-    c.height = img.offsetHeight
-    ctx.beginPath()
-    ctx.font = font
-    ctx.textBaseline = "top"
-    model?.detect(img).then((predictions) => {
-      predictions.forEach((prediction) => {
-        if (prediction.score < 0.8) return
-        // BBOX描画
-        const [x, y, width, height] = prediction.bbox
-        ctx.strokeStyle = "#00FFFF"
-        ctx.lineWidth = 4
-        ctx.strokeRect(x, y, width, height)
-        // クラス名描画
-        ctx.fillStyle = "#00FFFF"
-        const textWidth = ctx.measureText(prediction.class).width
-        const textHeight = parseInt(font, 10)
-        ctx.fillRect(x, y, textWidth + 50, textHeight + 4)
-        ctx.fillStyle = "#000000"
-        ctx.fillText(
-          `${prediction.class}(${Math.round(prediction.score * 100)}%)`,
-          x,
-          y
-        )
-      })
-    })
-  }
 
   return (
     <>
@@ -52,35 +19,7 @@ function App() {
               <InputFile setImgURL={setImgURL} />
             </Box>
             <Box>
-              {imgURL ? (
-                <div
-                  style={{
-                    width: "448px",
-                  }}
-                >
-                  <img
-                    id="img"
-                    alt="upload preview"
-                    style={{
-                      position: "absolute",
-                      zIndex: 1,
-                      width: "448px",
-                    }}
-                    src={imgURL}
-                    onLoad={onImageChange}
-                  />
-                  <canvas
-                    id="canvas"
-                    style={{
-                      position: "absolute",
-                      zIndex: 2,
-                      width: "448px",
-                    }}
-                  />
-                </div>
-              ) : (
-                <></>
-              )}
+              <DetectedResult model={model} imgURL={imgURL} />
             </Box>
           </VStack>
         ) : (
---
+++ tfjs_od/src/components/detectedResult.tsx
@@ -0,0 +1,80 @@
+import React from "react"
+import * as cocoSsd from "@tensorflow-models/coco-ssd"
+
+type DetectedResultProps = {
+  model: cocoSsd.ObjectDetection
+  imgURL: string
+}
+
+const DetectedResult = (props: DetectedResultProps) => {
+  const { model, imgURL } = props
+  const onImageChange = (e: any) => {
+    const img = e.target as HTMLImageElement
+    const c = document.getElementById("canvas") as HTMLCanvasElement
+    const ctx = c.getContext("2d")
+    if (!ctx) return
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    const font = "16px sans-serif"
+    c.width = img.offsetWidth
+    c.height = img.offsetHeight
+    ctx.beginPath()
+    ctx.font = font
+    ctx.textBaseline = "top"
+    model.detect(img).then((predictions) => {
+      predictions.forEach((prediction) => {
+        if (prediction.score < 0.8) return
+        // BBOX描画
+        const [x, y, width, height] = prediction.bbox
+        ctx.strokeStyle = "#00FFFF"
+        ctx.lineWidth = 4
+        ctx.strokeRect(x, y, width, height)
+        // クラス名描画
+        ctx.fillStyle = "#00FFFF"
+        const textWidth = ctx.measureText(prediction.class).width
+        const textHeight = parseInt(font, 10)
+        ctx.fillRect(x, y, textWidth + 50, textHeight + 4)
+        ctx.fillStyle = "#000000"
+        ctx.fillText(
+          `${prediction.class}(${Math.round(prediction.score * 100)}%)`,
+          x,
+          y
+        )
+      })
+    })
+  }
+  return (
+    <>
+      {imgURL ? (
+        <div
+          style={{
+            width: "448px",
+          }}
+        >
+          <img
+            id="img"
+            alt="upload preview"
+            style={{
+              position: "absolute",
+              zIndex: 1,
+              width: "448px",
+            }}
+            src={imgURL}
+            onLoad={onImageChange}
+          />
+          <canvas
+            id="canvas"
+            style={{
+              position: "absolute",
+              zIndex: 2,
+              width: "448px",
+            }}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  )
+}
+
+export default DetectedResult
